52f48a0d48830b0e0c3d9558152ba8c87ec2c78f
AliDemircix <alidemircix@gmail.com>
2022-01-23 18:44:14 +0100

    todos added

--- src/app.js
+++ src/app.js
@@ -10,3 +10,10 @@ const loadApp = () => {
 };
 
 window.addEventListener('load', loadApp);
+// 1- Find 10 question and make each of them 4 answer options
+// 2- Write a function that handle users selection and check if it is correct or not . 
+//      Create a handler folder and call the fuction in pages=>guestionPage.js
+// 3- Write a function to update score and add as eventlistener in next question button
+// 4- Write a function that checks if any answer selected or not. 
+//     If user not select any answer it should not pass next question.
+//     add it as eventlistener in next question button
