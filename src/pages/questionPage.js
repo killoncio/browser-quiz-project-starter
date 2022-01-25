71bd674bbfaa9da481c6508837569b75a02f0aaf
AliDemircix <alidemircix@gmail.com>
2022-01-25 19:32:46 +0100

    Handler Answers question completed

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -9,16 +9,12 @@ import { router } from '../router.js';
 
 export const initQuestionPage = (userInterface) => {
   const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
-
   const questionElement = getQuestionElement(currentQuestion.text);
   userInterface.appendChild(questionElement);
-
   const answersListElement = document.getElementById(ANSWERS_LIST_ID);
-
   for (const [key, answerText] of Object.entries(currentQuestion.answers)) {
     const answerElement = createAnswerElement(key, answerText);
     answersListElement.appendChild(answerElement);
-    //get answers handler
     answerElement.addEventListener('click',getAnswer)
   }
 // get next question handler
@@ -26,27 +22,14 @@ export const initQuestionPage = (userInterface) => {
     .getElementById(NEXT_QUESTION_BUTTON_ID)
     .addEventListener('click', nextQuestion)
 };
-let correctAnswers=0;
 // check answers if correct or not 
 const getAnswer = (e)=> {
   const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
   const answer = e.target.innerText;
   currentQuestion.selected=answer.charAt(0)
-  if(currentQuestion.selected===currentQuestion.correct){
-    console.log('selected correct answer')
-    e.target.style.backgroundColor='green'
-    // updated current answers counter
-    correctAnswers++
-    console.log('selected correct answer : '+correctAnswers)
-  }
-  else {
-    console.log('selected wrong answer')
-    e.target.style.backgroundColor='tomato'
-  }
+  currentQuestion.selected===currentQuestion.correct ? console.log('selected correct answer') : console.log('selected wrong answer')
 }
-
 const nextQuestion = () => {
   quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
-
   router('question');
 };
