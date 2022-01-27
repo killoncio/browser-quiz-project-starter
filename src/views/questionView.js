f50d28b298345c57a29fbb16c2a8a2b2cba3888a
stphn <cretestephen@gmail.com>
2022-01-27 21:44:06 +0100

    See Answer button creation code changed

--- src/views/questionView.js
+++ src/views/questionView.js
@@ -1,10 +1,9 @@
 'use strict';
 
 import {
-  ANSWERS_LIST_ID
-} from '../constants.js';
-import {
-  NEXT_QUESTION_BUTTON_ID
+    ANSWERS_LIST_ID,
+    NEXT_QUESTION_BUTTON_ID,
+    CHEAT_BUTTON_ID
 } from '../constants.js';
 
 /**
@@ -12,16 +11,17 @@ import {
  * @returns {Element}
  */
 export const getQuestionElement = (question) => {
-  const element = document.createElement('div');
+    const element = document.createElement('div');
 
-  // I use String.raw just to get fancy colors for the HTML in VS Code.
-  element.innerHTML = String.raw `
+    // I use String.raw just to get fancy colors for the HTML in VS Code.
+    element.innerHTML = String.raw `
     <h1>${question}</h1>
 
     <ul id="${ANSWERS_LIST_ID}"></ul>
 
     <button class="btn" id = "${NEXT_QUESTION_BUTTON_ID}"> Next question </button>
+    <button id= "${CHEAT_BUTTON_ID}">See Answer </button>
   `;
 
-  return element;
+    return element;
 };
