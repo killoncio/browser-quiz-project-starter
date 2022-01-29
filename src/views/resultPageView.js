e0ea180cb3b886b7c1c1c61bce661b4bf4484b8c
stphn <cretestephen@gmail.com>
2022-01-29 20:39:44 +0100

    resultPage titles added

--- src/views/resultPageView.js
+++ src/views/resultPageView.js
@@ -1,47 +1,48 @@
 import {
-  RESULT_CONTAINER_ID,
-  ANSWER_CONTAINER,
-  ANSWER_WRAPPER,
-  TEST_AGAIN_ID,
+    RESULT_CONTAINER_ID,
+    ANSWER_CONTAINER,
+    ANSWER_WRAPPER,
+    TEST_AGAIN_ID,
 } from "../constants.js";
 
 export const createTotalScoreElement = (
-  totalScore,
-  isSuccess = true,
-  questionCount
+    totalScore,
+    isSuccess = true,
+    questionCount
 ) => {
-  if (totalScore < 5) {
-    isSuccess = false;
-  }
-  const scoreElement = document.createElement("h1");
-  scoreElement.innerHTML = `Your SCORE is ${totalScore} out of ${questionCount}. You are ${
+    if (totalScore < 5) {
+        isSuccess = false;
+    }
+    const scoreElement = document.createElement("h1");
+    scoreElement.innerHTML = `Your SCORE is ${totalScore} out of ${questionCount}. You are ${
     isSuccess ? "Successful" : "Failed"
   }`;
-  return scoreElement;
+    return scoreElement;
 };
 export const createResultContainer = () => {
-  const resultContainer = document.createElement("div");
-  resultContainer.setAttribute("id", RESULT_CONTAINER_ID);
-  return resultContainer;
+    const resultContainer = document.createElement("div");
+    resultContainer.setAttribute("id", RESULT_CONTAINER_ID);
+    return resultContainer;
 };
-export const createAnswersContainer = (answers, correct = null) => {
-  const answersContainerElement = document.createElement("div");
-  answersContainerElement.setAttribute("class", ANSWER_CONTAINER);
-  answers.forEach((answer, i) => {
-    answersContainerElement.innerHTML += `<div class="${ANSWER_WRAPPER}"> 
+export const createAnswersContainer = (answers, wrong = null) => {
+    const answersContainerElement = document.createElement("div");
+    answersContainerElement.innerHTML = `<h2>${wrong ? 'Wrong Answers' : 'Correct Answers'}</h2>`;
+    answersContainerElement.setAttribute("class", ANSWER_CONTAINER);
+    answers.forEach((answer, i) => {
+        answersContainerElement.innerHTML += `<div class="${ANSWER_WRAPPER}"> 
       <h3>${i + 1}-${answer.title}</h3>
       <p><b>Your Answer:</b>${answer.userSelection}</p>
       <p>${
-        correct ? "<b>Correct Answer:</b> " + answer.correctSelection : ""
+        wrong ? "<b>Correct Answer:</b> " + answer.correctSelection : ""
       }</p></div>`;
-  });
-  return answersContainerElement;
+    });
+    return answersContainerElement;
 };
 export const createBtnElement = () => {
-  const btnElement = document.createElement("button");
-  btnElement.innerText = "Start Again";
-  btnElement.setAttribute("class", "btn");
-  btnElement.setAttribute("id", TEST_AGAIN_ID);
-  // btnElement.innerHTML = `<button id="${TEST_AGAIN_ID}" class ="btn">Test Again </button>`;
-  return btnElement;
+    const btnElement = document.createElement("button");
+    btnElement.innerText = "Start Again";
+    btnElement.setAttribute("class", "btn");
+    btnElement.setAttribute("id", TEST_AGAIN_ID);
+    // btnElement.innerHTML = `<button id="${TEST_AGAIN_ID}" class ="btn">Test Again </button>`;
+    return btnElement;
 };
